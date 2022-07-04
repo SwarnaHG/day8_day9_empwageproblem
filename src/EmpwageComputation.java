7ed19e537531568e0732ad58281790e1e554691c
SwarnaHG <swarnahg18@gmail.com>
2022-07-04 22:06:05 +0530

    checks emp is present or absent

---
+++ src/EmpwageComputation.java
@@ -0,0 +1,18 @@
+public class EmpwageComputation {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Employee Wage Computation in Java");
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
+    }
+}
+
