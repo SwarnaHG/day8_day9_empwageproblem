e5ea74d492805e03c6b355cc12dae059298694d2
SwarnaHG <swarnahg18@gmail.com>
2022-07-11 17:27:14 +0530

    uc8 calculate empwage for multiple companies

---
+++ src/EmpWageComputation8.java
@@ -0,0 +1,32 @@
+public class EmpWageComputation8 {
+    public static final int IS_PART_TIME= 1;
+    public static final int IS_FULL_TIME= 2;
+    public static int computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth) {
+        int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
+        while (totalEmpHrs <= maxHoursPerMonth) {
+            totalWorkingDays++;
+            int empCheck=(int) Math.floor(Math.random()*10)%3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                empHrs=4;
+                break;
+                case IS_FULL_TIME:
+                empHrs=8;
+                break;
+                default:
+                    empHrs=0;
+
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day#:" + totalWorkingDays + "Emp Hr:" +empHrs);
+
+        }
+        int totalEmpWage= totalEmpHrs + empRatePerHour;
+        System.out.println("Total Emp Wage for company:" +company+"is :"+ totalEmpWage);
+        return totalEmpWage;
+    }
+public static void main(String[]args) {
+        computeEmpWage("Dmart",20,2,10);
+        computeEmpWage("Relaince",10,4,20);
+}
+}
